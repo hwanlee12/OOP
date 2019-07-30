f5f34f31a68b54ae5860d836864df8408070265a
hwanlee12 <tmdghks912@naver.com>
2019-07-30 17:37:59 +0900

    db board

--- src/main/java/Board/main.java
+++ src/main/java/Board/main.java
@@ -1,13 +1,19 @@
 package Board;
 
+import org.w3c.dom.Text;
+
 import java.sql.*;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 
 public class main {
     public static void main(String args[]) throws ClassNotFoundException
     {
-        String ID;
-        String Password;
+        String ID = null;
+        String Password = null;
         int select = 0;
 
         while(true) {
@@ -46,7 +52,7 @@ public class main {
                     ID = in.nextLine();
                     Password = in.nextLine();
 
-                    while( rs.next() ) {
+                    while(rs.next()) {
                         String field1 = rs.getString("ID").trim();
                         String field2 = rs.getString("Password").trim();
                         if(field1.equals(ID) && field2.equals(Password)) {
@@ -87,6 +93,52 @@ public class main {
                 }
 
                 break;
+        } // 회원가입과 로그인
+
+        // 글쓰기 시작
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String connectionUrl = "jdbc:sqlserver://192.168.120.19:1433;" +
+                    "databaseName=pratice_j;user=testj;password=12345;";
+            Connection con = DriverManager.getConnection(connectionUrl);
+            Statement stmt = con.createStatement();
+
+            String user_id;
+            user_id = ID;
+            String title;
+            title = in.nextLine();
+
+            String message = "아 집가고 싶다......\n";
+
+            File file = new File("test1.txt");
+            FileWriter writer = null;
+
+            try {
+                writer = new FileWriter(file, false);
+                writer.write(message);
+                writer.flush();
+            }
+            catch(IOException e) {
+                e.printStackTrace();
+            }
+            finally {
+                try {
+                    if(writer != null) writer.close();
+                }
+                catch(IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            String sql = "insert into n_board(user_id, title, contents )";
+            sql += " VALUES(" + "'"+user_id+"', '"+title+"','"+file+"')";
+            stmt.executeUpdate(sql);
+
+            stmt.close();
+            con.close();
+        }
+        catch (SQLException sqle) {
+            System.out.println("SQLException : " + sqle);
         }
     }
 }
